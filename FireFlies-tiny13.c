7debdd8ca1c98277cef4c34ece4f3c4df498d283
Jason Schorr <jason@jasonschorr.com>
2010-12-13 07:47:27 -0500

    Minor change in source code, seems to be a weird bug that if you dont reset the leds they'll lock up eventually.

--- FireFlies-tiny13.c
+++ FireFlies-tiny13.c
@@ -52,20 +52,20 @@ int main (void){
 	TCCR0A |= ((1 << COM0A1) | (1 << COM0A0) // Set OC0A on Compare Match, clear OC0A at TOP
 				| (1 << COM0B1) | (1 << COM0B0) // Set OC0B on Compare Match, clear OC0B at TOP
 	 			| (1 << WGM01) | (1 << WGM00)); // WGM01 - WGM00 (set fast PWM)
- 
-	OCR0A = 255; 
-	OCR0B = 255; 
+
 
 	TCCR0B |= (1 << CS01); // Start timer at Fcpu / 8
 	
 	uint8_t rand = 0;
 
 	while(1){
-	_delay_ms(3000);
-	blink();
-	_delay_ms(1500);
-	blink2();
-	_delay_ms(900);
+		OCR0A = 255; 
+		OCR0B = 255; 
+		_delay_ms(2000);
+		blink();
+		_delay_ms(1500);
+		blink2();
+		_delay_ms(900);
 	}
 }
  
